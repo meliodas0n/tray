58be61641884aa6be61e122c1c3a8b8bf85c31c0
meliodas0n <mrunalnarayana@gmail.com>
2024-06-25 00:47:11 +0530

    something

--- src/run.py
+++ src/run.py
@@ -7,17 +7,14 @@ from fastapi.templating import Jinja2Templates
 
 app = FastAPI()
 
-static_files_path = f"{os.path.dirname(__file__)}/static"
-app.mount(
-    static_files_path,
-    StaticFiles(directory=static_files_path),
-    name="static",
-)
+static_files_path = f"{os.path.dirname('__filename__')}/static"
+app.mount(static_files_path, StaticFiles(directory=static_files_path), name="static")
 templates = Jinja2Templates(directory="templates")
-
 
 @app.get("/item/{id}", response_class=HTMLResponse)
 async def read_item(request: Request, id: str):
-    return templates.TemplateResponse(
-        request=request, name="item.html", context={"id": id}
-    )
+  return templates.TemplateResponse(request=request, name="item.html", context={"id" : id})
+
+
+if __name__ == "__main__":
+  app.run()
